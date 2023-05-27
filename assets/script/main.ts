349b56640b7fdd0389e530698ff996a31060f6c9
xpple <fred@xpple.dev>
2023-05-27 16:56:44 +0200

    Enable compression by default (now has full browser support)

--- assets/script/main.ts
+++ assets/script/main.ts
@@ -6,14 +6,7 @@ window.addEventListener("load", () => Utils.pasteToTextArea());
 
 const saveButton: HTMLLinkElement | null = document.querySelector("#save");
 if (saveButton == null) {
-    console.error("Save button is missing.");
-} else {
-    saveButton.onclick = () => Utils.copyToClipboard(false);
-}
-
-const compressSaveButton: HTMLLinkElement | null = document.querySelector("#compress-save");
-if (compressSaveButton == null) {
     console.error("Compress save button is missing.");
 } else {
-    compressSaveButton.onclick = () => Utils.copyToClipboard(true);
+    saveButton.onclick = () => Utils.copyToClipboard(true);
 }
